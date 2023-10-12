cd391bbfe4c98486a0b0c06b89c40d58f620a661
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-10-12 18:12:07 -0300

    Update api.ts

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: 'https://invitation-maker.vercel.app/api',
-  baseURL: 'http://192.168.100.115:3000/api',
+  baseURL: 'https://invitation-maker.vercel.app/api',
 })
