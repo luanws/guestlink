f4e006c07078528bbe5cb182478623636a2d77e2
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-10-13 01:22:49 -0300

    background color

--- mobile/src/providers/color-mode.tsx
+++ mobile/src/providers/color-mode.tsx
@@ -1,14 +1,18 @@
+import * as SystemUI from 'expo-system-ui'
 import { useColorMode } from 'native-base'
 import { PropsWithChildren, useEffect } from 'react'
 import usePersistedState from '../hooks/persisted-state'
+import { theme } from '../utils/theme'
 
 export function ColorModeProvider({ children }: PropsWithChildren) {
-  const [persistedColorMode] = usePersistedState<string>('colorMode', 'light')
+  const [colorMode] = usePersistedState<string>('colorMode', 'light')
   const { setColorMode } = useColorMode()
 
   useEffect(() => {
-    setColorMode(persistedColorMode)
-  }, [persistedColorMode])
+    setColorMode(colorMode)
+    const backgroundColor = colorMode === 'dark' ? theme.colors.gray['900'] : theme.colors.gray['100']
+    SystemUI.setBackgroundColorAsync(backgroundColor)
+  }, [colorMode])
 
   return children
 }
